a1c7fffd554e077918bf42bd0f2ed71e4d9ca8fc
Aapeli <git@aapelivuorinen.com>
2026-02-14 11:01:53 -0500

    Try to fix up integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -3,56 +3,46 @@ from datetime import datetime, timezone
 import pytest
 
 import pico_acme
-from pico_acme.route53 import route53_upsert_cleanup
 
 
 @pytest.mark.integration
-def test_full_certificate_issuance(hosted_zone_id, test_domain):
+def test_full_certificate_issuance_and_reissue(hosted_zone_id, test_domain):
+    from pico_acme.route53 import route53_upsert_cleanup
+
     # 1. Register account on staging
     acme_client = pico_acme.register_account("test@pico-acme-test.a19e.net", agree_tos=True, staging=True)
 
-    # 2. Generate ECDSA key
+    # 2. Generate ECDSA key and CSR
     key_pem = pico_acme.make_ecdsa_key()
-
-    # 3. Create CSR
     csr_pem = pico_acme.make_csr(key_pem, [test_domain])
 
-    # 4. Get DNS callbacks
+    # 3. Issue certificate
     upsert_dns, cleanup_dns = route53_upsert_cleanup(hosted_zone_id)
-
-    # 5. Issue certificate
     fullchain_pem = pico_acme.perform_dns01(acme_client, test_domain, csr_pem, upsert_dns, cleanup_dns)
 
-    # 6. Verify returned PEM is a valid cert chain
+    # 4. Verify returned PEM is a valid cert chain
     assert isinstance(fullchain_pem, str)
     assert "BEGIN CERTIFICATE" in fullchain_pem
 
-    # 7. Check expiry is in the future
+    # 5. Check expiry is in the future
     expiry = pico_acme.get_expiry(fullchain_pem)
     assert isinstance(expiry, datetime)
     assert expiry > datetime.now(timezone.utc)
 
-    # 8. Should not need renewal (just issued)
+    # 6. Should not need renewal (just issued)
     assert pico_acme.should_renew(fullchain_pem) is False
 
-
-@pytest.mark.integration
-def test_serialize_deserialize_and_reissue(hosted_zone_id, test_domain):
-    # 1. Register account on staging
-    acme_client = pico_acme.register_account("test@pico-acme-test.a19e.net", agree_tos=True, staging=True)
-
-    # 2. Serialize and deserialize
+    # 7. Serialize and deserialize the account
     data = pico_acme.serialize_account(acme_client)
     restored_client = pico_acme.deserialize_account(data, staging=True)
+    assert restored_client.net.account.uri == acme_client.net.account.uri
 
-    # 3. Issue a cert with the deserialized client
-    key_pem = pico_acme.make_ecdsa_key()
-    csr_pem = pico_acme.make_csr(key_pem, [test_domain])
-    upsert_dns, cleanup_dns = route53_upsert_cleanup(hosted_zone_id)
-    fullchain_pem = pico_acme.perform_dns01(restored_client, test_domain, csr_pem, upsert_dns, cleanup_dns)
+    # 8. Reissue a cert with the restored client
+    key_pem2 = pico_acme.make_ecdsa_key()
+    csr_pem2 = pico_acme.make_csr(key_pem2, [test_domain])
+    upsert_dns2, cleanup_dns2 = route53_upsert_cleanup(hosted_zone_id)
+    fullchain_pem2 = pico_acme.perform_dns01(restored_client, test_domain, csr_pem2, upsert_dns2, cleanup_dns2)
 
-    # 4. Verify cert is valid
-    assert isinstance(fullchain_pem, str)
-    assert "BEGIN CERTIFICATE" in fullchain_pem
-    expiry = pico_acme.get_expiry(fullchain_pem)
-    assert expiry > datetime.now(timezone.utc)
+    # 9. Verify second cert is valid
+    assert "BEGIN CERTIFICATE" in fullchain_pem2
+    assert pico_acme.get_expiry(fullchain_pem2) > datetime.now(timezone.utc)
